2223c2612f3239349880d5c1942ed33fbe79f652
Irina <irina@lanl.gov>
2021-01-27 15:25:57 -0700

    updating kokkos test

--- flecsi/exec/test/kokkos.cc
+++ flecsi/exec/test/kokkos.cc
@@ -38,36 +38,35 @@ struct canon : topo::specialization<topo::canonical, canon> {
 canon::slot canonical;
 canon::cslot coloring;
 
-const field<double>::definition<canon, canon::cells> cell_field;
+const field<int>::definition<canon, canon::cells> cell_field;
 
 const int pvalue = 35;
 
 int
-init(field<double>::accessor<wo> c) {
+init(field<int>::accessor<wo> c) {
   UNIT {
     flecsi::exec::parallel_for(
-      c.span(), KOKKOS_LAMBDA(auto & cv) { cv = pvalue; }, std::string("test"));
+      c.span(), KOKKOS_LAMBDA(auto & cv) {cv = pvalue;}, std::string("test"));
   };
 } // init
 
 void
-local_kokkos(field<double>::accessor<rw> c) {
+local_kokkos(field<int>::accessor<rw> c) {
     // Parallel for
     flecsi::exec::parallel_for(
       c.span(),
-      KOKKOS_LAMBDA(auto cv) { assert(cv == pvalue); },
-      std::string("pfor1"));
-
+      KOKKOS_LAMBDA(auto cv) { assert(cv == pvalue); }, std::string("pfor1"));
+   
     forall(cv, c.span(), "pfor2") {
       assert(cv == pvalue);
     }; // forall
-
     // Reduction
     std::size_t res = exec::parallel_reduce<exec::fold::sum, std::size_t>(
       c.span(),
       KOKKOS_LAMBDA(auto cv, std::size_t & up) { up += cv; },
       std::string("pred1"));
     assert(pvalue * c.span().size() == res);
+
     res = reduceall(cv, up, c.span(), exec::fold::sum, std::size_t, "pred2") {
       up += cv;
     };
@@ -85,9 +84,13 @@ kokkos_driver() {
     coloring.allocate(filename);
     canonical.allocate(coloring.get());
     const auto pressure = cell_field(canonical);
+#if defined(__NVCC__) || defined(__CUDACC__)
     flecsi::execute<init, toc>(pressure);
-
     flecsi::execute<local_kokkos, toc>(pressure);
+#else
+    flecsi::execute<init>(pressure);
+    flecsi::execute<local_kokkos>(pressure);
+#endif
   };
 } // driver
 
