88bf92762924260309d7de9aacebed70d5eb37d2
Davis Herring <herring@lanl.gov>
2021-02-06 00:19:09 -0700

    Update generic interface

--- flecsi/io.hh
+++ flecsi/io.hh
@@ -20,47 +20,39 @@
 namespace flecsi::io {
 
 #ifdef DOXYGEN // these are implemented per-backend
-typedef unspecified hdf5_t, hdf5_region_t, launch_space_t;
+typedef unspecified hdf5_region_t, launch_space_t;
+
+struct hdf5_t {
+  static hdf5_t create(const std::string &);
+  static hdf5_t open(const std::string &);
+  hdf5_t(const char *, int num_files);
+  /// Must not be called repeatedly.
+  /// \return whether the file was successfully closed
+  bool close();
+  bool write_string(const std::string & group,
+    const std::string & dataset,
+    const std::string &);
+  bool read_string(const std::string & group,
+    const std::string & dataset,
+    std::string &);
+  bool create_dataset(const std::string &, hsize_t size);
+};
 #endif
 
-// TODO: make these member functions
-hdf5_t init_hdf5_file(const char *, int num_files);
-bool create_hdf5_file(hdf5_t &, int file_idx);
-bool open_hdf5_file(hdf5_t &, int file_idx);
-bool close_hdf5_file(hdf5_t &);
-bool create_datasets_for_regions(hdf5_t & hdf5_file, int file_idx);
-
-bool write_string_to_hdf5_file(hdf5_t &,
-  int file_idx,
-  const char * group_name,
-  const char * dataset_name,
-  const std::string &,
-  size_t);
-bool read_string_from_hdf5_file(hdf5_t &,
-  int file_idx,
-  const char * group_name,
-  const char * dataset_name,
-  std::string &);
-
-void add_regions(hdf5_t &, std::vector<hdf5_region_t> &);
-void generate_hdf5_files(hdf5_t &);
-
-void checkpoint_data(hdf5_t &,
+void checkpoint_data(const std::string &,
   launch_space_t,
   std::vector<hdf5_region_t> &,
   bool attach);
-void recover_data(hdf5_t &,
+void recover_data(const std::string &,
   launch_space_t,
   std::vector<hdf5_region_t> &,
   bool attach);
 
 #ifdef DOXYGEN
 struct io_interface_t {
-  void add_process_topology(hdf5_t &);
-  void checkpoint_process_topology(hdf5_t &);
-  void checkpoint_index_topology_field(hdf5_t &, const field_reference_t &);
-  void recover_process_topology(hdf5_t &);
-  void recover_index_topology_field(hdf5_t &, const field_reference_t &);
+  void add_process_topology(int num_files);
+  void checkpoint_process_topology(const std::string &);
+  void recover_process_topology(const std::string &);
 };
 #endif
 
