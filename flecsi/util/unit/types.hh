2dda3cf83f50bb4621b33f06379cf02afb005b15
Davis Herring <herring@lanl.gov>
2021-05-06 10:00:53 -0600

    Report streamable operands to failed comparisons
    This makes the string assertions usable

--- flecsi/util/unit/types.hh
+++ flecsi/util/unit/types.hh
@@ -28,6 +28,35 @@ inline log::devel_tag unit_tag("unit");
 namespace util {
 namespace unit {
 
+namespace detail {
+template<class T, class = void>
+struct maybe_value {
+  static const char * get(const T &, const char * what) {
+    return what;
+  }
+};
+template<class T>
+struct maybe_value<T,
+  decltype(void(std::declval<std::ostream &>() << std::declval<const T &>()))> {
+  static const T & get(const T & t, const char *) {
+    return t;
+  }
+};
+template<class T>
+decltype(auto)
+stream(const T & t, const char * what) {
+  return maybe_value<T>::get(t, what);
+}
+
+template<class T>
+struct not_fn { // default-constructed std::not_fn
+  template<class... AA>
+  bool operator()(AA &&... aa) const {
+    return T()(std::forward<AA>(aa)...);
+  }
+};
+} // namespace detail
+
 struct state_t {
 
   state_t(std::string name) {
@@ -57,18 +86,44 @@ struct state_t {
     return error_stream_;
   } // stream
 
+  template<bool A, class... CC>
+  void fail(const char * file, int line, const CC &... cc) {
+    result_ = 1;
+    if(A)
+      error_stream_ << FLOG_OUTPUT_LTRED("ASSERT FAILED") << ": '";
+    else
+      error_stream_ << FLOG_OUTPUT_YELLOW("EXPECT FAILED") << ": '";
+    (error_stream_ << ... << cc)
+      << "' at " << FLOG_COLOR_BROWN << file << ':' << line << ' ';
+  }
   template<bool A>
   bool test(bool b, const char * what, const char * file, int line) {
-    if(!b) {
-      result_ = 1;
-      if(A)
-        error_stream_ << FLOG_OUTPUT_LTRED("ASSERT FAILED") << ": '";
-      else
-        error_stream_ << FLOG_OUTPUT_YELLOW("EXPECT FAILED") << ": '";
-      error_stream_ << what << "' at " << FLOG_COLOR_BROWN << file << ':'
-                    << line << ' ';
-    }
+    if(!b)
+      fail<A>(file, line, what);
     return b;
+  }
+  template<class C, bool A, class T, class U>
+  bool compare(const T & t,
+    const U & u,
+    const char * ts,
+    const char * op,
+    const char * us,
+    const char * sfx,
+    const char * file,
+    int line) {
+    const bool ret = C()(t, u);
+    if(!ret)
+      fail<A>(file,
+        line,
+        '(',
+        detail::stream(t, ts),
+        ')',
+        op,
+        '(',
+        detail::stream(u, us),
+        ')',
+        sfx);
+    return ret;
   }
 
   template<class F>
@@ -89,57 +144,31 @@ private:
 
 }; // struct state_t
 
-template<typename T1, typename T2>
-inline bool
-test_equal(const T1 & v1, const T2 & v2) {
-  return v1 == v2;
-}
-
-template<typename T1, typename T2>
-inline bool
-test_less(const T1 & v1, const T2 & v2) {
-  return v1 < v2;
-}
-
-template<typename T1, typename T2>
-inline bool
-test_less_equal(const T1 & v1, const T2 & v2) {
-  return test_less(v1, v2) || test_equal(v1, v2);
-}
-
-template<typename T1, typename T2>
-inline bool
-test_greater(const T1 & v1, const T2 & v2) {
-  return v1 > v2;
-}
-
-template<typename T1, typename T2>
-inline bool
-test_greater_equal(const T1 & v1, const T2 & v2) {
-  return test_greater(v1, v2) || test_equal(v1, v2);
-}
-
-inline bool
-string_compare(const char * lhs, const char * rhs) {
-  if(lhs == nullptr) {
-    return rhs == nullptr;
+struct string_compare {
+  using not_fn = detail::not_fn<string_compare>;
+  bool operator()(const char * lhs, const char * rhs) const {
+    if(lhs == nullptr) {
+      return rhs == nullptr;
+    }
+    if(rhs == nullptr) {
+      return false;
+    }
+    return strcmp(lhs, rhs) == 0;
   }
-  if(rhs == nullptr) {
-    return false;
-  }
-  return strcmp(lhs, rhs) == 0;
-} // string_compare
+};
 
-inline bool
-string_case_compare(const char * lhs, const char * rhs) {
-  if(lhs == nullptr) {
-    return rhs == nullptr;
+struct string_case_compare {
+  using not_fn = detail::not_fn<string_case_compare>;
+  bool operator()(const char * lhs, const char * rhs) const {
+    if(lhs == nullptr) {
+      return rhs == nullptr;
+    }
+    if(rhs == nullptr) {
+      return false;
+    }
+    return strcasecmp(lhs, rhs) == 0;
   }
-  if(rhs == nullptr) {
-    return false;
-  }
-  return strcasecmp(lhs, rhs) == 0;
-} // string_case_compare
+};
 
 } // namespace unit
 } // namespace util
@@ -155,56 +184,70 @@ string_case_compare(const char * lhs, const char * rhs) {
 
 #define UNIT_TTYPE(type) ::flecsi::util::demangle(typeid(type).name())
 
-#define CHECK(ret, A, condition, what)                                         \
-  if(auto_unit_state.test<A>(condition, what, __FILE__, __LINE__))             \
+#define CHECK(ret, f, ...)                                                     \
+  if(auto_unit_state.f(__VA_ARGS__, __FILE__, __LINE__))                       \
     ;                                                                          \
   else                                                                         \
     ret auto_unit_state >>= auto_unit_state.stringstream()
 
-#define ASSERT_TRUE(c) CHECK(return, true, c, #c)
-#define EXPECT_TRUE(c) CHECK(, false, c, #c)
+#define ASSERT_TRUE(c) CHECK(return, test<true>, c, #c)
+#define EXPECT_TRUE(c) CHECK(, test<false>, c, #c)
 
 #define ASSERT_FALSE(c) ASSERT_TRUE(!(c))
 #define EXPECT_FALSE(c) EXPECT_TRUE(!(c))
 
-#define ASSERT_CMP(x, y, neg, f)                                               \
-  ASSERT_TRUE(neg ::flecsi::util::unit::test_##f(x, y))
-#define EXPECT_CMP(x, y, neg, f)                                               \
-  EXPECT_TRUE(neg ::flecsi::util::unit::test_##f(x, y))
+#define COMMA , // relies on CHECK invoking no other macros
+#define CHECK_CMP(ret, cmp, A, x, y, op, sfx)                                  \
+  CHECK(ret, compare<cmp COMMA A>, x, y, #x, #op, #y, sfx)
 
-#define ASSERT_EQ(x, y) ASSERT_CMP(x, y, , equal)
-#define EXPECT_EQ(x, y) EXPECT_CMP(x, y, , equal)
-#define ASSERT_NE(x, y) ASSERT_CMP(x, y, !, equal)
-#define EXPECT_NE(x, y) EXPECT_CMP(x, y, !, equal)
-#define ASSERT_LT(x, y) ASSERT_CMP(x, y, , less)
-#define EXPECT_LT(x, y) EXPECT_CMP(x, y, , less)
-#define ASSERT_LE(x, y) ASSERT_CMP(x, y, , less_equal)
-#define EXPECT_LE(x, y) EXPECT_CMP(x, y, , less_equal)
-#define ASSERT_GT(x, y) ASSERT_CMP(x, y, , greater)
-#define EXPECT_GT(x, y) EXPECT_CMP(x, y, , greater)
-#define ASSERT_GE(x, y) ASSERT_CMP(x, y, , greater_equal)
-#define EXPECT_GE(x, y) EXPECT_CMP(x, y, , greater_equal)
+#define ASSERT_CMP(x, y, cmp, op, sfx)                                         \
+  CHECK_CMP(return, cmp, true, x, y, op, sfx)
+#define EXPECT_CMP(x, y, cmp, op, sfx) CHECK_CMP(, cmp, false, x, y, op, sfx)
 
-#define CHECK_STR(ret, A, x, y, neg, f, op, sfx)                               \
-  CHECK(ret, A, neg ::flecsi::util::unit::f(x, y), a #op b sfx)
-
-#define ASSERT_STR(x, y, neg, f, op, sfx)                                      \
-  CHECK_STR(return, true, x, y, neg, f, op, sfx)
-#define EXPECT_STR(x, y, neg, f, op, sfx)                                      \
-  CHECK_STR(, false, x, y, neg, f, op, sfx)
-
-#define ASSERT_STREQ(x, y) ASSERT_STR(x, y, , string_compare, ==, "")
-#define EXPECT_STREQ(x, y) EXPECT_STR(x, y, , string_compare, ==, "")
-#define ASSERT_STRNE(x, y) ASSERT_STR(x, y, !, string_compare, !=, "")
-#define EXPECT_STRNE(x, y) EXPECT_STR(x, y, !, string_compare, !=, "")
+#define ASSERT_EQ(x, y) ASSERT_CMP(x, y, ::std::equal_to<>, ==, "")
+#define EXPECT_EQ(x, y) EXPECT_CMP(x, y, ::std::equal_to<>, ==, "")
+#define ASSERT_NE(x, y) ASSERT_CMP(x, y, ::std::not_equal_to<>, !=, "")
+#define EXPECT_NE(x, y) EXPECT_CMP(x, y, ::std::not_equal_to<>, !=, "")
+#define ASSERT_LT(x, y) ASSERT_CMP(x, y, ::std::less<>, <, "")
+#define EXPECT_LT(x, y) EXPECT_CMP(x, y, ::std::less<>, <, "")
+#define ASSERT_LE(x, y) ASSERT_CMP(x, y, ::std::less_equal<>, <=, "")
+#define EXPECT_LE(x, y) EXPECT_CMP(x, y, ::std::less_equal<>, <=, "")
+#define ASSERT_GT(x, y) ASSERT_CMP(x, y, ::std::greater<>, >, "")
+#define EXPECT_GT(x, y) EXPECT_CMP(x, y, ::std::greater<>, >, "")
+#define ASSERT_GE(x, y) ASSERT_CMP(x, y, ::std::greater_equal<>, >=, "")
+#define EXPECT_GE(x, y) EXPECT_CMP(x, y, ::std::greater_equal<>, >=, "")
+#define ASSERT_STREQ(x, y)                                                     \
+  ASSERT_CMP(x, y, ::flecsi::util::unit::string_compare, ==, "")
+#define EXPECT_STREQ(x, y)                                                     \
+  EXPECT_CMP(x, y, ::flecsi::util::unit::string_compare, ==, "")
+#define ASSERT_STRNE(x, y)                                                     \
+  ASSERT_CMP(x, y, ::flecsi::util::unit::string_compare::not_fn, !=, "")
+#define EXPECT_STRNE(x, y)                                                     \
+  EXPECT_CMP(x, y, ::flecsi::util::unit::string_compare::not_fn, !=, "")
 #define ASSERT_STRCASEEQ(x, y)                                                 \
-  ASSERT_STR(x, y, , string_case_compare, ==, " (case insensitive)")
+  ASSERT_CMP(x,                                                                \
+    y,                                                                         \
+    ::flecsi::util::unit::string_case_compare,                                 \
+    ==,                                                                        \
+    " (case insensitive)")
 #define EXPECT_STRCASEEQ(x, y)                                                 \
-  EXPECT_STR(x, y, , string_case_compare, ==, " (case insensitive)")
+  EXPECT_CMP(x,                                                                \
+    y,                                                                         \
+    ::flecsi::util::unit::string_case_compare,                                 \
+    ==,                                                                        \
+    " (case insensitive)")
 #define ASSERT_STRCASENE(x, y)                                                 \
-  ASSERT_STR(x, y, !, string_case_compare, !=, " (case insensitive)")
+  ASSERT_CMP(x,                                                                \
+    y,                                                                         \
+    ::flecsi::util::unit::string_case_compare::not_fn,                         \
+    !=,                                                                        \
+    " (case insensitive)")
 #define EXPECT_STRCASENE(x, y)                                                 \
-  EXPECT_STR(x, y, !, string_case_compare, !=, " (case insensitive)")
+  EXPECT_CMP(x,                                                                \
+    y,                                                                         \
+    ::flecsi::util::unit::string_case_compare::not_fn,                         \
+    !=,                                                                        \
+    " (case insensitive)")
 
 // Provide access to the output stream to allow user to capture output
 #define UNIT_CAPTURE()                                                         \
