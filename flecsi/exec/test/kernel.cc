d30bfd55df84f5ff9243e0aa381e746a339a7b38
Sumathi Lakshmiranganatha <sumathil@lanl.gov>
2021-09-27 14:36:49 -0600

    added tests for range policies

--- flecsi/exec/test/kernel.cc
+++ flecsi/exec/test/kernel.cc
@@ -24,7 +24,7 @@ const intN::definition<topo::index> array_field;
 
 void
 modify(intN::accessor<wo> a) {
-  forall(i, flecsi::exec::range_policy{util::span(*a)}, "modify") {
+  forall(i, util::span(*a), "modify") {
     i = 3;
   };
 }
@@ -37,6 +37,40 @@ check(intN::accessor<ro> a) {
     }
   };
 }
+
+void
+modify_policy(intN::accessor<wo> a) {
+  forall(j, flecsi::exec::range_policy(util::span(*a)), "modify_policy") {
+    j = 3;
+  };
+}
+
+int
+check_policy(intN::accessor<ro> a) {
+  UNIT {
+    for(auto j : util::span(*a)) {
+      EXPECT_EQ(j, 3);
+    }
+  };
+}
+
+void
+modify_bound(intN::accessor<wo> a) {
+  forall(i, (flecsi::exec::range_bound{util::span(*a), 0, 10}), "modify_policy") {
+    i = 3;
+  };
+}
+
+int
+check_bound(intN::accessor<ro> a) {
+  UNIT {
+    for(auto i : util::span(*a)) {
+      EXPECT_EQ(i, 3);
+    }
+  };
+}
+
+
 
 int
 reduce_vec(intN::accessor<ro> a) {
@@ -55,6 +89,10 @@ kernel_driver() {
     const auto ar = array_field(process_topology);
     execute<modify, default_accelerator>(ar);
     EXPECT_EQ(test<check>(ar), 0);
+    execute<modify_policy, default_accelerator>(ar);
+    EXPECT_EQ(test<check_policy>(ar), 0);
+    execute<modify_bound, default_accelerator>(ar);
+    EXPECT_EQ(test<check_bound>(ar), 0);
     EXPECT_EQ((test<reduce_vec, default_accelerator>(ar)), 0);
   };
 } // kernel_driver
