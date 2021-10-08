0807ff513fd6618ce11610d8fa320b1465ea6ca7
Sumathi Lakshmiranganatha <sumathil@lanl.gov>
2021-10-07 18:20:04 -0600

    Added unit tests for bound check

--- flecsi/exec/test/kernel.cc
+++ flecsi/exec/test/kernel.cc
@@ -54,23 +54,6 @@ check_policy(intN::accessor<ro> a) {
   };
 }
 
-void
-modify_bound(intN::accessor<wo> b) {
-  forall(
-    j, (flecsi::exec::range_policy{util::span(*b), 0, 5}), "modify_bound") {
-    j = 2;
-  };
-}
-
-int
-check_bound(intN::accessor<ro> b) {
-  UNIT {
-    for(auto j : util::span(*b).first(5)) {
-      EXPECT_EQ(j, 2);
-    }
-  };
-}
-
 int
 reduce_vec(intN::accessor<ro> a) {
   UNIT {
@@ -82,18 +65,52 @@ reduce_vec(intN::accessor<ro> a) {
   };
 }
 
+void
+modify_bound(intN::accessor<wo> a) {
+  forall(
+    j, (flecsi::exec::range_policy{util::span(*a), 0, 5}), "modify_first") {
+    j = 2;
+  };
+  forall(
+    j, (flecsi::exec::range_policy{util::span(*a), 5, 10}), "modify_last") {
+    j = 5;
+  };
+}
+
 int
-reduce_vec_bound(intN::accessor<ro> b) {
+check_bound(intN::accessor<ro> a) {
   UNIT {
-    size_t res = reduceall(j,
+    for(auto j : util::span(*a).subspan(0, 5)) {
+      EXPECT_EQ(j, 2);
+    }
+    for(auto j : util::span(*a).subspan(5, 5)) {
+      EXPECT_EQ(j, 5);
+    }
+  };
+}
+
+int
+reduce_vec_bound(intN::accessor<ro> a) {
+  UNIT {
+    size_t res_first = reduceall(j,
       up,
-      (flecsi::exec::range_policy{util::span(*b), 0, 5}),
+      (flecsi::exec::range_policy{util::span(*a), 0, 5}),
       exec::fold::sum,
       size_t,
-      "reduce_bound") {
+      "reduce_first") {
       up += j;
     };
-    EXPECT_EQ(res, 2 * 5);
+    EXPECT_EQ(res_first, 2 * 5);
+
+    size_t res_last = reduceall(j,
+      up,
+      (flecsi::exec::range_policy{util::span(*a), 5, 10}),
+      exec::fold::sum,
+      size_t,
+      "reduce_last") {
+      up += j;
+    };
+    EXPECT_EQ(res_last, 5 * 5);
   };
 }
 
@@ -101,15 +118,14 @@ int
 kernel_driver() {
   UNIT {
     const auto ar = array_field(process_topology);
-    const auto br = array_field(process_topology);
     execute<modify, default_accelerator>(ar);
     EXPECT_EQ(test<check>(ar), 0);
     execute<modify_policy, default_accelerator>(ar);
     EXPECT_EQ(test<check_policy>(ar), 0);
     EXPECT_EQ((test<reduce_vec, default_accelerator>(ar)), 0);
-    execute<modify_bound, default_accelerator>(br);
-    EXPECT_EQ(test<check_bound>(br), 0);
-    EXPECT_EQ((test<reduce_vec_bound, default_accelerator>(br)), 0);
+    execute<modify_bound, default_accelerator>(ar);
+    EXPECT_EQ(test<check_bound>(ar), 0);
+    EXPECT_EQ((test<reduce_vec_bound, default_accelerator>(ar)), 0);
   };
 } // kernel_driver
 
