7a0f27650bee89a136fd54f4e0a2ba63c187fc06
Ben Bergen <bergen@lanl.gov>
2021-10-19 17:08:47 -0400

    Removed CMake Namespace Pollution

--- cmake/parmetis.cmake
+++ cmake/parmetis.cmake
@@ -15,14 +15,14 @@
 option(ENABLE_PARMETIS "Enable ParMETIS" OFF)
 
 if(ENABLE_PARMETIS)
-  find_package(METIS 5.1)
+  find_package(METIS ${METIS_VERSION})
 
   if(NOT METIS_FOUND)
     message(FATAL_ERROR "METIS is required for this configuration")
   endif()
 
   set(PARMETIS_TEST_RUNS TRUE)
-  find_package(ParMETIS 4.0)
+  find_package(ParMETIS ${PARMETIS_VERSION})
 
   if(NOT ParMETIS_FOUND)
     message(FATAL_ERROR "ParMETIS is required for this configuration")
