191538dc90ffb6a27387c9dd5a402784e48db021
Andrew Reisner <areisner@lanl.gov>
2021-12-08 12:08:47 -0700

    Attempt to fix parameters for MPI_Scatter.
    The parameters to MPI_Scatter looked incorrect in one of the static
    branches of one_to_allv.  This attempts to fix them.

--- flecsi/util/mpi.hh
+++ flecsi/util/mpi.hh
@@ -252,10 +252,10 @@ one_to_allv(F const & f, MPI_Comm comm = MPI_COMM_WORLD) {
     if(!rank)
       for(int r = 1; r < size; ++r)
         send.push_back(f(r, size));
-    test(MPI_Scatter(MPI_IN_PLACE,
-      0,
-      MPI_DATATYPE_NULL,
-      send.data(),
+    test(MPI_Scatter(send.data(),
+      1,
+      type<return_type>(),
+      rank ? send.data() : MPI_IN_PLACE,
       1,
       type<return_type>(),
       0,
