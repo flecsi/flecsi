9cf602b8e6b3c56508157cfcf822c22c728f8c40
Davis Herring <herring@lanl.gov>
2022-05-18 22:26:37 -0600

    Document ENABLE_FLOG consequences

--- flecsi/util/unit.hh
+++ flecsi/util/unit.hh
@@ -16,6 +16,7 @@ namespace flecsi {
 namespace unit {
 /// \defgroup unit Unit Testing
 /// Unit test framework much like Google Test but with task support.
+/// Output is via \ref flog.
 /// \ingroup utils
 /// \{
 
