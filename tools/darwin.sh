20acbdba276c1b0743242c73526f19849131d456
Richard Berger <rberger@lanl.gov>
2022-07-13 17:58:16 -0600

    New version of tools/darwin.sh

--- tools/darwin.sh
+++ tools/darwin.sh
@@ -33,48 +33,91 @@ cd build
 # Define an installation directory.
 FLECSI_INSTALL="$HOME/flecsi-inst"
 
+# Set GCC version we're going to use
+GCC_VERSION=11.1.0
+
 # Download a version of Spack known to work with FleCSI and activate it.
 pushd "$HOME"
-git clone https://github.com/spack/spack.git
-cd spack
-git switch -c origin/v0.17.2
-git rev-parse HEAD
+if [ ! -d spack ]; then
+  git clone https://github.com/spack/spack.git
+  cd spack
+  git switch releases/v0.18
+  git rev-parse HEAD
+else
+  cd spack
+  echo "Found existing Spack install in ~/spack"
+  git fetch origin
+  if [ "$(git rev-parse HEAD)" != "$(git rev-parse origin/releases/v0.18)" ]; then
+    echo "ERROR: The current checkout does not match origin/releases/v0.18!"
+    echo
+    echo "Please update manually with:"
+    echo " git -C ~/spack switch releases/v0.18"
+    echo " git -C ~/spack pull"
+    echo
+    echo "WARNING: This may invalidate other Spack environments that rely on" \
+         "this Spack instance!"
+    exit 1
+  fi
+fi
 set +v
 source "$HOME/spack/share/spack/setup-env.sh"
 set -v
 popd
 
-# Load a newer CMake and versions of GCC known to work with FleCSI.
-# Expose these -- and whatever else happense to be sitting around -- as Spack
-# externals.
-module load cmake/3.19.2
-module load gcc/9.4.0
-spack compiler find
-spack external find
-spack config remove packages:python   # Provides only a partial Sphinx
-spack config remove packages:libtool  # Seems incomplete
 
-# Install FleCSI's dependencies with Spack.  The various Sphinx packages lead
-# to a mess of dependencies that confuses Spack.  We temporarily specify
-# "concretization: together" to get those installed then revert to the default
-# of "concretization: separately".
-#
-# This also builds a version of MPICH in Spack since the ones on Darwin do not include
-# an mpiexec/mpirun that works.
+# Create new Spack environment and activate it
+spack env remove -y flecsi-mpich || true
 spack env create flecsi-mpich
 spack env activate flecsi-mpich
-echo "  concretization: together" >> "$HOME/spack/var/spack/environments/flecsi-mpich/spack.yaml"
+
+# Load GCC version known to work with FleCSI
+# and make it visible to Spack
+set +v
+module load gcc/${GCC_VERSION}
+spack compiler find
+set -v
+
+# ignore configuration in ~/.spack to avoid conflicts
+export SPACK_DISABLE_LOCAL_CONFIG=true
+
+# allow spack to dynamically concretize packages together instead of separately.
+# this avoids some installation errors due to multiple python packages depending
+# on different versions of a dependency
+spack config add concretizer:unify:when_possible
+spack config add concretizer:reuse:false
+spack config add packages:all:compiler:["gcc@${GCC_VERSION}"]
+
+# add FleCSI spack package repository
 spack repo add ../spack-repo
-spack add py-sphinx py-sphinx-rtd-theme py-recommonmark
-spack install
-sed -i -e 's/concretization: together/concretization: separately/' "$HOME/spack/var/spack/environments/flecsi-mpich/spack.yaml"
-spack install graphviz +poppler
-spack install --only dependencies flecsi%gcc@9.4.0 backend=legion +hdf5 +kokkos +flog ^mpich@3.4.2%gcc@9.4.0+hydra+romio~verbs device=ch4 ^legion network=gasnet conduit=mpi build_type=Debug
-spack load py-sphinx py-sphinx-rtd-theme py-recommonmark
+
+# add documentation dependencies
+spack add py-sphinx py-sphinx-rtd-theme py-recommonmark graphviz +poppler
+
+# On Darwin we have a Spack upstream that already has prebuilt dependencies
+DARWIN_SPACK_UPSTREAM=/projects/flecsi-devel/gitlab/spack-upstream/current
+
+if [ -d "$DARWIN_SPACK_UPSTREAM" ] && [ -x "${DARWIN_SPACK_UPSTREAM}" ]; then
+  # add spack upstream if accessible
+  spack config add upstreams:default:install_tree:${DARWIN_SPACK_UPSTREAM}/opt/spack/
+  cp ${DARWIN_SPACK_UPSTREAM}/etc/spack/packages.yaml $HOME/spack/etc/spack/
+else
+  # Otherwise, load a compatible cmake and expose whatever else happens to be
+  # sitting around as Spack externals
+  module load cmake/3.19.2
+  spack external find
+fi
+
+# install packages added so far
+spack install -j $(nproc)
+
+# Install FleCSI's dependencies with Spack.
+# This also builds a version of MPICH in Spack since the ones on Darwin do not include
+# an mpiexec/mpirun that works.
+spack install -j $(nproc) --only dependencies flecsi%gcc@${GCC_VERSION} backend=legion +hdf5 +kokkos +flog ^mpich@3.4.2%gcc@${GCC_VERSION}+hydra+romio~verbs device=ch4 ^legion network=gasnet conduit=mpi build_type=Debug
 
 # Build, test, and install FleCSI.
 ../tools/configure gnu legion -DCMAKE_INSTALL_PREFIX="$FLECSI_INSTALL"
-make VERBOSE=1 -j
+make VERBOSE=1 -j $(nproc)
 make test
 make doc
 make install
@@ -88,3 +131,14 @@ cd build
 cmake -DFleCSI_DIR="$FLECSI_INSTALL/lib64/cmake/FleCSI" ..
 make
 cd ../..
+
+# Build complete
+set +v
+echo "BUILD COMPLETE"
+echo
+echo "To continue working in the same Spack environment, execute the following commands:"
+echo
+echo " source \$HOME/spack/share/spack/setup-env.sh"
+echo " spack env activate flecsi-mpich"
+echo " module load gcc/${GCC_VERSION}"
+echo
