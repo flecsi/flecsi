851885d466dcd250d8fe6a3692833505e3efb8e9
Davis Herring <herring@lanl.gov>
2023-08-24 19:27:46 -0600

    Clarify helper template descriptions

--- flecsi/topo/core.hh
+++ flecsi/topo/core.hh
@@ -131,20 +131,20 @@ struct specialization_base {
 
   /// A connectivity specification.
   /// \tparam V input index space
-  /// \tparam T see \c to
+  /// \tparam T a \c to list
   template<auto V, class T>
   using from = util::key_type<V, T>;
   /// A special-entities specification.
   /// \tparam V subject index space
-  /// \tparam T see \c has
+  /// \tparam T a \c has list
   template<auto V, class T>
   using entity = util::key_type<V, T>;
-  /// Entity lists.
-  /// \tparam V entity list enumerators
+  /// A list of enumerators for which to store data.
+  /// \tparam V often index spaces
   template<auto... V>
   using has = util::constants<V...>;
-  /// Output index spaces.
-  /// \tparam V index spaces
+  /// A list of index spaces to use as output.
+  /// \tparam V enumerators
   template<auto... V>
   using to = util::constants<V...>;
   /// Container.
