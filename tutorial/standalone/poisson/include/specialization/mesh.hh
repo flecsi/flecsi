d3c7d4f64bbe58defec6efe47051ad6458d34607
Davis Herring <herring@lanl.gov>
2024-05-20 09:09:40 -0600

    Remove unused is_boundary

--- tutorial/standalone/poisson/include/specialization/mesh.hh
+++ tutorial/standalone/poisson/include/specialization/mesh.hh
@@ -132,44 +132,6 @@ struct mesh : flecsi::topo::specialization<flecsi::topo::narray, mesh> {
       return (A == x_axis ? xdelta() : ydelta()) * global_id<A>(i);
     }
 
-    template<axis A, boundary BD>
-    bool is_boundary(std::size_t i) {
-
-      auto const loff =
-        B::template offset<mesh::vertices, A, base::domain::logical>();
-      auto const lsize =
-        B::template size<mesh::vertices, A, base::domain::logical>();
-      const bool l = B::template is_low<mesh::vertices, A>();
-      const bool h = B::template is_high<mesh::vertices, A>();
-
-      if(l && h) { /* degenerate */
-        if constexpr(BD == boundary::low) {
-          return i == loff;
-        }
-        else {
-          return i == (lsize + loff - 1);
-        }
-      }
-      else if(l) {
-        if constexpr(BD == boundary::low) {
-          return i == loff;
-        }
-        else {
-          return false;
-        }
-      }
-      else if(h) {
-        if constexpr(BD == boundary::low) {
-          return false;
-        }
-        else {
-          return i == (lsize + loff - 1);
-        }
-      }
-      else { /* interior */
-        return false;
-      }
-    } // is_boundary
   }; // struct interface
 
   /*--------------------------------------------------------------------------*
