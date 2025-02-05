7be9175a72cc09f2806636457ee09324dab2479c
Davis Herring <herring@lanl.gov>
2025-02-05 07:43:06 -0700

    Restore documentation lost in 732465cb4

--- flecsi/exec/future.hh
+++ flecsi/exec/future.hh
@@ -30,6 +30,7 @@ struct future<Return, exec::launch_type_t::index> {
   /// Wait on all the tasks to finish.
   void wait(bool silence_warnings = false);
   /// Get the result of one of the tasks.
+  /// Note that all processes must select the same \a index.
   Return get(Color index = 0, bool silence_warnings = false);
   /// Get the number of tasks.
   Color size() const;
