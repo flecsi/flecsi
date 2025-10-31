89418cf691399c945b7d0cd8b03ab56769783204
Davis Herring <herring@lanl.gov>
2025-10-31 13:32:57 -0600

    Combine checks/conversions as make_parameter
    Make error message more precise, partly to improve blame

--- flecsi/exec/launch.hh
+++ flecsi/exec/launch.hh
@@ -217,27 +217,6 @@ struct consistent_variants
       S2> {};
 } // namespace detail
 
-template<bool M, class... PP>
-void
-check_parameters() {
-  if constexpr(!M) {
-    static_assert((!std::is_rvalue_reference_v<PP> && ...),
-      "only MPI tasks can accept rvalue references");
-    static_assert((std::is_const_v<std::remove_reference_t<const PP>> && ...),
-      "only MPI tasks can accept non-const references");
-    static_assert(
-      ((!std::is_pointer_v<PP> || std::is_const_v<std::remove_pointer_t<PP>> ||
-        std::is_function_v<std::remove_pointer_t<PP>>)&&...),
-      "only MPI tasks can accept non-const pointers");
-    static_assert((std::is_move_constructible_v<std::decay_t<PP>> && ...),
-      "only MPI tasks can accept non-movable parameters");
-    static_assert((std::is_copy_constructible_v<PP> && ...),
-      "only MPI tasks can accept non-copyable parameters by value");
-  }
-  static_assert((!detail::bad_accessor<M, std::decay_t<PP>> && ...),
-    "only MPI tasks without ghosts can accept non-portable field accessors");
-}
-
 // Replaces certain task arguments before conversion to the parameter type.
 template<class P, class T>
 decltype(auto)
@@ -247,13 +226,40 @@ replace_argument(T && t) {
 }
 
 namespace detail {
+template<bool M, bool R, class P, class A>
+decltype(auto)
+make_parameter(A && a) {
+  using PD = std::decay_t<P>;
+  if constexpr(!M) {
+    static_assert(!std::is_rvalue_reference_v<P>,
+      "only MPI tasks can accept rvalue references");
+    static_assert(std::is_const_v<std::remove_reference_t<const P>>,
+      "only MPI tasks can accept non-const references");
+    static_assert(!std::is_pointer_v<P> ||
+                    std::is_const_v<std::remove_pointer_t<P>> ||
+                    std::is_function_v<std::remove_pointer_t<P>>,
+      "only MPI tasks can accept non-const pointers");
+    static_assert(std::is_move_constructible_v<PD>,
+      "only MPI tasks can accept (references to) non-movable parameters");
+    static_assert(std::is_copy_constructible_v<P>,
+      "only MPI tasks can accept non-copyable parameters by value");
+  }
+  static_assert(!detail::bad_accessor<M, PD>,
+    "only MPI tasks without ghosts can accept non-portable field accessors");
+  const auto f = [&a]() -> decltype(auto) {
+    return exec::replace_argument<P>(std::forward<A>(a));
+  };
+  if constexpr(R)
+    return f();
+  else
+    return [&f]() -> PD { return f(); }();
+}
+
 template<bool M, bool R, class... PP, class... AA>
 auto
 make_parameters(std::tuple<PP...> * /* to deduce PP */, AA &&... aa) {
-  check_parameters<M, PP...>();
-  return std::tuple<std::conditional_t<R,
-    decltype(exec::replace_argument<PP>(std::forward<AA>(aa))),
-    std::decay_t<PP>>...>(exec::replace_argument<PP>(std::forward<AA>(aa))...);
+  return std::tuple<decltype(make_parameter<M, R, PP>(std::forward<AA>(
+    aa)))...>(make_parameter<M, R, PP>(std::forward<AA>(aa))...);
 }
 } // namespace detail
 template<bool M, class P, bool R = M, class... AA>
