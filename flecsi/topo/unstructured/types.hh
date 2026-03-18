9ea48963f724db2053085508719b472d6d847cce
Davis Herring <herring@lanl.gov>
2026-03-17 23:16:30 -0600

    Fix Doxygen groups

--- flecsi/topo/unstructured/types.hh
+++ flecsi/topo/unstructured/types.hh
@@ -24,9 +24,9 @@
 namespace flecsi {
 namespace topo {
 
+namespace unstructured_impl {
 /// \addtogroup unstructured
 /// \{
-namespace unstructured_impl {
 
 using entity_index_space = std::size_t;
 
@@ -135,10 +135,12 @@ operator<<(std::ostream & stream, index_color const & ic) {
   return stream;
 }
 
+/// \}
 } // namespace unstructured_impl
 
 /// Specialization-independent definitions.
 /// Name as \c base in an \c unstructured specialization.
+/// \ingroup unstructured
 struct unstructured_base : base {
   /// The type for specifying an index space for a single color.
   using index_color = unstructured_impl::index_color;
@@ -454,6 +456,7 @@ namespace unstructured_impl {
   @tparam NF    Number of privileges for connectivity field.
   @param  mconn A multi-accessor to the connectivity field.
   @param  connectivities
+  \ingroup unstructured
  */
 template<PrivilegeCount NF>
 void
@@ -475,7 +478,6 @@ init_connectivity(
 /// \endcond
 } // namespace unstructured_impl
 
-/// \}
 } // namespace topo
 
 } // namespace flecsi
