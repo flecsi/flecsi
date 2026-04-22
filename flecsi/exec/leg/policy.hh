c568722d09fb972eed5772baf663127608e7a46c
Davis Herring <herring@lanl.gov>
2026-04-22 14:07:34 -0600

    Rename task_wrapper.hh to task.hh

--- flecsi/exec/leg/policy.hh
+++ flecsi/exec/leg/policy.hh
@@ -7,7 +7,7 @@
 #include "flecsi/exec/future.hh"
 #include "flecsi/exec/launch.hh"
 #include "flecsi/exec/leg/fold.hh"
-#include "flecsi/exec/leg/task_wrapper.hh"
+#include "flecsi/exec/leg/task.hh"
 #include "flecsi/exec/leg/tracer.hh"
 #include "flecsi/exec/params.hh"
 #include "flecsi/run/backend.hh"
